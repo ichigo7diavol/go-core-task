850d695ae8d0fa79c6ecd72b0cf2f4bb7ef274b4
irudichph <irudichph@gmail.com>
2025-10-25 20:50:51 +0300

    [features/task-9] realization and tests

--- 9/main.go
+++ 9/main.go
@@ -1,5 +1,37 @@
+// #### Задание 9
+// Сделать конвейер чисел
+// Даны два канала.
+// В первый пишутся числа типа uint8. Нужно, чтобы числа читались из первого канала по мере поступления,
+// затем эти числа должны преобразовываться в float64 и возводиться в куб и результат записывался во второй канал.
+// Напишите main функцию, в которой протестируете весь вышеописанный функционал. Выведите результаты на экран.
+// * Напишите unit тесты к созданным функциям
+
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func CubePipeline(in <-chan uint8, out chan<- float64) {
+	for n := range in {
+		out <- float64(n * n * n)
+	}
+	close(out)
+}
+
+func main() {
+	in := make(chan uint8)
+	out := make(chan float64)
+
+	go CubePipeline(in, out)
+
+	go func() {
+		for _, v := range []uint8{1, 2, 3, 4, 5, 10} {
+			in <- v
+		}
+		close(in)
+	}()
+	for result := range out {
+		fmt.Println(result)
+	}
 }
--- 9/main_test.go
+++ 9/main_test.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCubePipeline(t *testing.T) {
+	in := make(chan uint8)
+	out := make(chan float64)
+
+	go CubePipeline(in, out)
+
+	go func() {
+		for _, v := range []uint8{1, 2, 3, 4} {
+			in <- v
+		}
+		close(in)
+	}()
+
+	var results []float64
+	for v := range out {
+		results = append(results, v)
+	}
+
+	expected := []float64{1, 8, 27, 64}
+
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("ожидалось %v, получено %v", expected, results)
+	}
+}
+
+func TestCubePipelineEmpty(t *testing.T) {
+	in := make(chan uint8)
+	out := make(chan float64)
+
+	go CubePipeline(in, out)
+	close(in)
+
+	var results []float64
+	for v := range out {
+		results = append(results, v)
+	}
+
+	if len(results) != 0 {
+		t.Errorf("ожидался пустой срез, получено %v", results)
+	}
+}
