01eea55b09fa55fc133d331a71204eb0d8d64b93
irudichph <irudichph@gmail.com>
2025-10-25 19:50:31 +0300

    [features/task-6] realization and tests

--- 7/main.go
+++ 7/main.go
@@ -1,5 +1,67 @@
+// #### Задание 7
+// Напишите программу на Go, которая сливает N каналов в один.
+// * Напишите unit тесты к созданным функциям
+
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func Merge[T any](chans ...<-chan T) <-chan T {
+	out := make(chan T)
+
+	go func() {
+		defer close(out)
+
+		done := make(chan struct{})
+		active := len(chans)
+
+		for _, ch := range chans {
+			go func(c <-chan T) {
+				for v := range c {
+					out <- v
+				}
+				done <- struct{}{}
+			}(ch)
+		}
+		for i := 0; i < active; i++ {
+			<-done
+		}
+		close(done)
+	}()
+	return out
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+
+	go func() {
+		defer close(ch1)
+		for i := 0; i < 3; i++ {
+			ch1 <- i
+		}
+	}()
+
+	go func() {
+		defer close(ch2)
+		for i := 10; i < 13; i++ {
+			ch2 <- i
+		}
+	}()
+
+	go func() {
+		defer close(ch3)
+		for i := 100; i < 103; i++ {
+			ch3 <- i
+		}
+	}()
+
+	merged := Merge(ch1, ch2, ch3)
+
+	for v := range merged {
+		fmt.Println(v)
+	}
 }
--- 7/main_test.go
+++ 7/main_test.go
@@ -1,1 +1,70 @@
 package main
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func makeIntChan(vals ...int) <-chan int {
+	ch := make(chan int, len(vals))
+	for _, v := range vals {
+		ch <- v
+	}
+	close(ch)
+	return ch
+}
+
+func TestMerge_Basic(t *testing.T) {
+	ch1 := makeIntChan(1, 2)
+	ch2 := makeIntChan(3, 4)
+	ch3 := makeIntChan(5, 6)
+
+	out := Merge(ch1, ch2, ch3)
+
+	var result []int
+	for v := range out {
+		result = append(result, v)
+	}
+
+	sort.Ints(result)
+	expected := []int{1, 2, 3, 4, 5, 6}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ожидалось %v, получено %v", expected, result)
+	}
+}
+
+func TestMerge_EmptyChannels(t *testing.T) {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	close(ch1)
+	close(ch2)
+
+	out := Merge(ch1, ch2)
+
+	var result []int
+	for v := range out {
+		result = append(result, v)
+	}
+
+	if len(result) != 0 {
+		t.Errorf("ожидалось пусто, получено %v", result)
+	}
+}
+
+func TestMerge_SingleChannel(t *testing.T) {
+	ch := makeIntChan(7, 8, 9)
+
+	out := Merge(ch)
+
+	var result []int
+	for v := range out {
+		result = append(result, v)
+	}
+
+	expected := []int{7, 8, 9}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ожидалось %v, получено %v", expected, result)
+	}
+}
