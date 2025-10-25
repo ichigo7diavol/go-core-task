22d597268defff0a281087b05b96ce79a6949b31
irudichph <irudichph@gmail.com>
2025-10-25 17:53:23 +0300

    [features/task-2] realization and tests

--- 2/main.go
+++ 2/main.go
@@ -1,5 +1,71 @@
+// #### Задание 2
+// 1. Создайте слайс целых чисел originalSlice, содержащий 10 произвольных значений, которые генерируются случайным
+// образом (при каждом запуске должны получаться новые значения)
+// 2. Напишите функцию sliceExample, которая принимает слайс и возвращает новый слайс, содержащий только четные числа из исходного слайса.
+// 3. Напишите функцию addElements, которая принимает слайс и число. Функция должна добавлять это число в конец слайса и возвращать новый слайс.
+// 4. Напишите функцию copySlice, которая принимает слайс и возвращает его копию. Убедитесь, что изменения в оригинальном слайсе не влияют на его копию.
+// 5. Напишите функцию removeElement, которая принимает слайс и индекс элемента, который нужно удалить. Функция должна возвращать новый слайс без элемента по указанному индексу.
+// 6. Напишите main функцию, в которой протестируете все вышеописанные функции. Выведите результаты на экран.
+// * Напишите unit тесты к созданным функциям
+// Примечание.
+// В качестве originalSlice можно использовать ```originalSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}```
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func sliceExample(slice []int) []int {
+	var result []int
+	for _, v := range slice {
+		if v%2 == 0 {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func addElements(slice []int, num int) []int {
+	return append(slice, num)
+}
+
+func copySlice(slice []int) []int {
+	copySlice := make([]int, len(slice))
+	copy(copySlice, slice)
+	return copySlice
+}
+
+func removeElement(slice []int, index int) []int {
+	if index < 0 || index >= len(slice) {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func main() {
+	originalSlice := make([]int, 10)
+	for i := range originalSlice {
+		originalSlice[i] = rand.Intn(100)
+	}
+
+	fmt.Println("Оригинальный слайс:", originalSlice)
+
+	evenSlice := sliceExample(originalSlice)
+	fmt.Println("Четные числа:", evenSlice)
+
+	addedSlice := addElements(originalSlice, 777)
+	fmt.Println("После добавления числа 777:", addedSlice)
+
+	copiedSlice := copySlice(originalSlice)
+	fmt.Println("Копия слайса:", copiedSlice)
+
+	originalSlice[0] = 999
+	fmt.Println("Измененный оригинал:", originalSlice)
+	fmt.Println("Копия после изменения оригинала:", copiedSlice)
+
+	indexToRemove := 3
+	removedSlice := removeElement(originalSlice, indexToRemove)
+	fmt.Printf("После удаления элемента с индексом %d: %v\n", indexToRemove, removedSlice)
 }
--- 2/main_test.go
+++ 2/main_test.go
@@ -1,1 +1,54 @@
-package main_test
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSliceExample(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5, 6}
+	expected := []int{2, 4, 6}
+	result := sliceExample(input)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ожидалось %v, получено %v", expected, result)
+	}
+}
+
+func TestAddElements(t *testing.T) {
+	input := []int{1, 2, 3}
+	expected := []int{1, 2, 3, 10}
+	result := addElements(input, 10)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ожидалось %v, получено %v", expected, result)
+	}
+}
+
+func TestCopySlice(t *testing.T) {
+	input := []int{5, 6, 7}
+	copy := copySlice(input)
+	input[0] = 100
+
+	if reflect.DeepEqual(input, copy) {
+		t.Errorf("копия изменилась вместе с оригиналом")
+	}
+}
+
+func TestRemoveElement(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5}
+	expected := []int{1, 2, 4, 5}
+	result := removeElement(input, 2)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ожидалось %v, получено %v", expected, result)
+	}
+}
+
+func TestRemoveElement_OutOfBounds(t *testing.T) {
+	input := []int{1, 2, 3}
+	result := removeElement(input, 10)
+	if !reflect.DeepEqual(result, input) {
+		t.Errorf("при неверном индексе результат должен быть тем же")
+	}
+}
